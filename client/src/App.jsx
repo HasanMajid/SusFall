1e28592d217419b803a080afe3ec347c0f074cfe
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-18 10:22:08 -0500

    added socket provider and game page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Routes, Link, BrowserRouter as Router } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
 import Home from "./screens/Home";
 import Messages from "./screens/Messages";
 import Tutorial from "./screens/Tutorial";
-import Layout from "./components/layout/Layout";
+import Game from "./screens/Game";
+
+import { SocketProvider } from "./contexts/SocketContext";
 
 const App = () => {
   return (
     <>
       <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/tutorial" element={<Tutorial />} />
-          </Routes>
-        </Layout>
+        <SocketProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/tutorial" element={<Tutorial />} />
+              <Route path="/game/:room" element={<Game />} />
+            </Routes>
+          </Layout>
+        </SocketProvider>
       </Router>
     </>
   );
