35788a4747becdbfaf41fe70b38777372107f438
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-18 10:23:35 -0500

    formatted code and host game button creates room

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Button,
   ButtonGroup,
@@ -8,18 +9,43 @@ import {
   Spacer,
 } from "@chakra-ui/react";
 
+import { SocketContext } from "../contexts/SocketContext";
+
 function Home() {
+  const socket = useContext(SocketContext);
+  const createRoom = async () => {
+    const roomID = crypto.randomUUID();
+    await socket.emit("create_room", roomID);
+    navigate("/game/" + roomID);
+  };
+
+
+
+  const navigate = useNavigate();
   return (
     <Flex align={"center"} flexDir={"column"}>
       <Heading color="facebook.400" m={20} fontSize={100}>
         Susfall{" "}
       </Heading>
       <Stack spacing={4} direction="column" align="center">
-        <Button colorScheme="facebook" fontSize={30} size="lg" _hover={{opacity:0.85}}>
+        <Button
+          colorScheme="facebook"
+          fontSize={30}
+          size="lg"
+          _hover={{ opacity: 0.85 }}
+          onClick={() => {
+            createRoom();
+          }}
+        >
           Host New Game
         </Button>
         <Spacer />
-        <Button colorScheme="facebook" fontSize={30} size="lg" _hover={{opacity:0.85}}>
+        <Button
+          colorScheme="facebook"
+          fontSize={30}
+          size="lg"
+          _hover={{ opacity: 0.85 }}
+        >
           Join Game
         </Button>
       </Stack>
