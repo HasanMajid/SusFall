a25e46c086046aff4d1095c23b4549bfcb535923
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-18 10:24:13 -0500

    created listeners for creating a game with players

--- server/index.js
+++ server/index.js
@@ -15,12 +15,49 @@ const io = new Server(server, {
   },
 });
 
+const rooms = {};
+
 io.on("connection", (socket) => {
-  console.log(`User connected: ${socket.id}`);
+  socket.on("create_room", async (room) => {
+    if (socket.host) {
+      console.log("deleting roomID:", socket.roomID);
+      io.socketsLeave(socket.roomID);
+      delete rooms[socket.roomID];
+    }
+    socket.host = true;
+    socket.roomID = room;
+    console.log("created game, host:", socket.host);
+    rooms[room] = {};
+  });
+
+  socket.on("join_room", async (data) => {
+    console.log(socket.id);
+    if (rooms[data.room]) {
+      rooms[data.room][socket.id] = { name: data.name, score: 0 };
+      await socket.join(data.room);
+      io.to(data.room).emit("players", Object.values(rooms[data.room]));
+    } else {
+      socket.emit("error_message", "Room Does not Exist");
+    }
+    console.log("Joined game, host:", socket.host);
+  });
 
   socket.on("send_message", (data) => {
-    // console.log(data);
-    socket.broadcast.emit("receive_message", data)
+    console.log(data);
+  });
+
+  socket.on("disconnect", () => {
+    for (const room in rooms) {
+      if (socket.id in rooms[room]) {
+        delete rooms[room][socket.id];
+        io.to(room).emit("players", Object.values(rooms[room]));
+
+        if (Object.keys(rooms[room].length === 0)) {
+          delete rooms[room];
+        }
+        break;
+      }
+    }
   });
 });
 
