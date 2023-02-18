eb180cf7e55f43c0e2c3c8cd961b05e7d05955cd
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-18 10:17:54 -0500

    created component for name input

---
+++ client/src/components/game/NameInput.jsx
@@ -0,0 +1,60 @@
+import { useRef, useState } from "react";
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+
+function NameInput({ setName }) {
+  const [currentName, setCurrentName] = useState('')
+  return (
+    <Flex
+      m={"auto"}
+      mt={100}
+      justifyContent={"center"}
+      flexDir={"column"}
+      width={200}
+      height={160}
+      borderWidth={2}
+      borderRadius={10}
+      backgroundColor={"facebook.700"}
+      shadow={"md"}
+    >
+      <Heading fontSize={20} fontWeight={"bold"} color={"white"} mt={1}>
+        Enter name to continue:
+      </Heading>
+      <Flex m={2}>
+        <Input
+          size="sm"
+          borderRadius={10}
+          placeholder={"name"}
+          backgroundColor={"white"}
+          fontWeight={"bold"}
+          _placeholder={{ color: "facebook.600", fontWeight: "bold" }}
+          onChange={(e) => {
+            e.preventDefault();
+            setCurrentName(e.target.value)
+          }}
+          color={"facebook.600"}
+        />
+      </Flex>
+      <Button
+        m={"auto"}
+        w={"auto"}
+        mb={1}
+        mt={2}
+        onClick={(e) => {
+          setName(currentName)
+        }}
+      >
+        Enter
+      </Button>
+    </Flex>
+  );
+}
+
+export default NameInput;
