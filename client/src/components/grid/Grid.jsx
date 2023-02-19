893512785a4b6f636a944205fedff8a50593253f
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-18 21:59:42 -0500

    renders list of location images

--- client/src/components/grid/Grid.jsx
+++ client/src/components/grid/Grid.jsx
@@ -1,21 +1,16 @@
 import React, { useEffect } from "react";
-import { SimpleGrid, Box, Image, Center } from "@chakra-ui/react";
-import GameCard from "./card/Card";
+import { SimpleGrid, Box, Image, Center, Card } from "@chakra-ui/react";
+import GameCard from "./card/GameCard";
+import locations, { locationNames } from "../../constants/locations";
 
 function Grid() {
   return (
-    <SimpleGrid spacing={5} m={"auto"} minChildWidth={130} maxW={800} >
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      {/* <Box bg="tomato" height="80px"></Box>
-    <Box bg="tomato" height="80px"></Box>
-    <Box bg="tomato" height="80px"></Box>
-    <Box bg="tomato" height="80px"></Box>
-    <Box bg="tomato" height="80px"></Box> */}
+    <SimpleGrid spacing={5} m={"auto"} minChildWidth={180} maxW={1000}>
+      {Object.values(locations).map((location, index) => {
+        return (
+          <GameCard key={index} img={location} name={locationNames[index]} />
+        );
+      })}
     </SimpleGrid>
   );
 }
