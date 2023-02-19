ef20e81a7cdc51a75660dfb4ea0b29c9bdd1936a
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-18 21:59:57 -0500

    displays grid with random image

--- client/src/screens/Tutorial.jsx
+++ client/src/screens/Tutorial.jsx
@@ -1,10 +1,30 @@
-import { Flex, Heading, SimpleGrid, Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  SimpleGrid,
+  Box,
+  Image,
+  Center,
+} from "@chakra-ui/react";
 import React from "react";
 import Grid from "../components/grid/Grid";
+import locations, { locationNames } from "../constants/locations";
 
 function Tutorial() {
+  const image = locationNames[Math.floor(Math.random() * locationNames.length)];
+  console.log(locations[image]);
   return (
-    <Grid/>
+    <Center flexDir={"column"} m={5}>
+      <Image
+        src={locations[image]}
+        w={500}
+        shadow={"dark-lg"}
+        borderWidth={2}
+        borderRadius={10}
+      />
+      <Heading m={2}>{image}</Heading>
+      <Grid />
+    </Center>
   );
 }
 
