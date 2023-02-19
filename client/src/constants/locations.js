8af63d1dd8e64b544a179c10982f01b30ba0c7d0
Hasan Majid <hasanmajid3.14@hotmail.com>
2023-02-18 21:57:42 -0500

    created constants for images and location names

---
+++ client/src/constants/locations.js
@@ -0,0 +1,65 @@
+import airplane from "../assets/airplane.jpg";
+import museum from "../assets/art museum.jpg";
+import bank from "../assets/bank.jpg";
+import beach from "../assets/beach.jpg";
+import theatre from "../assets/broadway theatre.jpg";
+import casino from "../assets/casino.jpg";
+import circus from "../assets/circus.jpg";
+import classroom from "../assets/classroom.jpg";
+import embassy from "../assets/embassy.jpg";
+import gasStation from "../assets/gas-station.jpg";
+import hospital from "../assets/hospital.jpg";
+import hotel from "../assets/hotel.jpg";
+import militaryArmy from "../assets/millitary army.jpg";
+import movieStudio from "../assets/movie studio.jpg";
+import passengerTrain from "../assets/passenger train.jpg";
+import policeDep from "../assets/police department.jpg";
+import restaurant from "../assets/restaurant.jpg";
+import serviceStation from "../assets/service station.jpg";
+import spaceStation from "../assets/space station.jpg";
+
+const locations = {
+  airplane,
+  museum,
+  bank,
+  beach,
+  theatre,
+  casino,
+  circus,
+  classroom,
+  embassy,
+  gasStation,
+  hospital,
+  hotel,
+  militaryArmy,
+  movieStudio,
+  passengerTrain,
+  policeDep,
+  restaurant,
+  serviceStation,
+  spaceStation,
+};
+
+export const locationNames = [
+  "airplane",
+  "museum",
+  "bank",
+  "beach",
+  "theatre",
+  "casino",
+  "circus",
+  "classroom",
+  "embassy",
+  "gasStation",
+  "hospital",
+  "hotel",
+  "militaryArmy",
+  "movieStudio",
+  "passengerTrain",
+  "policeDep",
+  "restaurant",
+  "serviceStation",
+  "spaceStation",
+];
+
+export default locations;
